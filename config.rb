3933c6141056e8616946dd393fb85c43d3ed0fd7
Taras Bilous <tbilous@yahoo.com>
2015-10-17 12:20:34 +0300

    initial commit

---
+++ config.rb
@@ -0,0 +1,34 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "scripts"
+sourcemap     = true
+#output_style  = :compressed
+sass_options  = { cache: false }
+line_comments = false
+
+#var compass = require('cannot load such file -- gulp-compass'),
+#autoprefixer = require('gulp-autoprefixer');
+
+#require 'autoprefixer-rails'
+
+#on_stylesheet_saved do |file|ff
+#  css = File.read(file)
+#  map = file + '.map'
+
+#  if File.exists? map
+#    result = AutoprefixerRails.process(css,
+#      from: file,
+#      to:   file,
+#      map:  { prev: File.read(map), inline: false })
+#    File.open(file, 'w') { |io| io << result.css }
+#    File.open(map,  'w') { |io| io << result.map }
+#  else
+#    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+#  end
+#end
