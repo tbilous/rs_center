f95b9edf892e5fd595c448b22f108a17126d3109
Taras Bilous <tbilous@yahoo.com>
2015-10-22 10:57:58 +0300

    autoprefixer, fa-plus, spy-menu

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+'use strict';
+var path = require('path');
+function absolutePath(file) {
+    return path.join(__dirname, file);
+}
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        autoprefixer: { // https://github.com/nDmitry/grunt-autoprefixer
+            options: {
+                browsers: ['> 1%', 'bb 10', 'ie 8', 'ie 9'],
+                remove: true
+                /*                map: {
+                 inline: false
+                 }*/
+
+            },
+            no_dest: {
+                src: 'css/styles.css' // output file
+            }
+
+        },
+
+        cssmin: {
+            options: {},
+            minify: {
+                src: ['css/styles.css'],
+                dest: absolutePath('css/styles.min.css')
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['css/styles.css*'],
+                tasks: ['default']
+            }
+        }
+
+    });
+
+
+    // register task
+    grunt.registerTask('default', ['autoprefixer', 'cssmin']);
+    grunt.registerTask('dev', ['watch']);
+
+};
--- script/plugins.js
+++ script/plugins.js
@@ -42,14 +42,60 @@ $(document).ready(function () {
             var children = $(this).parent('li.parent_li').find(' > ul > li');
             if (children.is(":visible")) {
                 children.hide('fast');
-                $(this).attr('title', 'Expand this branch').find(' > i').addClass("glyphicon-plus").removeClass('glyphicon-minus');
+                $(this).attr('title', 'Expand this branch').find(' > i').addClass("fa-plus").removeClass('fa-minus');
             } else {
                 children.show('fast');
-                $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass(" glyphicon-plus");
+                $(this).attr('title', 'Collapse this branch').find(' > i').addClass('fa-minus').removeClass("fa-plus");
             }
             e.stopPropagation();
         });
     });
+// Cache selectors
+    var lastId,
+        topMenu = $("#top-menu"),
+        topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+        menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function () {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
+        });
 
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+    menuItems.click(function (e) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        e.preventDefault();
+    });
+
+// Bind to scroll
+    $(window).scroll(function () {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function () {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href=#" + id + "]").parent().addClass("active");
+        }
+    });
 
 });
