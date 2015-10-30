75e355d710b208f8e25655bbab4308693799aba0
Taras Bilous <tbilous@yahoo.com>
2015-10-30 10:50:16 +0200

    played grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,3 +49,82 @@ module.exports = function (grunt) {
     grunt.registerTask('dev', ['watch']);
 
 };
+/*
+
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+
+        // Reference package.json
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Compass
+        compass: {
+            dist: {
+                options: {config: 'config.rb'}
+            }
+        },
+
+        // Auto Prefixer
+        autoprefixer: {
+            dist: {
+                options: {
+                    browsers: ['last 3 version', '> 1%', 'ie 8']
+                },
+                files: {
+                    'css/style-prefixed.css': ['css/styles.css']
+                }
+            }
+        },
+
+        // Minify CSS
+        cssmin: {
+            combine: {
+                files: {
+                    'css/styles.min.css': ['css/style-prefixed.css']
+                }
+            }
+        },
+
+        // Minify JS
+        uglify: {
+            my_target: {
+                files: {
+                    'js/scripts-min.js': ['js/src/theme.js']
+                }
+            }
+        },
+
+        // Watch
+        watch: {
+            compass: {
+                files: 'sass/!*.scss',
+                tasks: ['compass']
+            },
+
+            csspostprocess: {
+                files: 'css/styles.css',
+                tasks: ['autoprefixer', 'cssmin']
+            },
+
+            jsminify: {
+                files: 'script/!*.js',
+                tasks: ['uglify']
+            },
+
+            livereload: {
+                options: {livereload: true},
+                files: ['css/!*.css', 'js/!*.js', '*.html', 'img/!*']
+            }
+        }
+
+    });
+
+    grunt.registerTask('default', ['watch']);
+};*/
--- config.rb
+++ config.rb
@@ -15,17 +15,18 @@ sass_options  = { cache: false }
 line_comments = false
 
 
-#on_stylesheet_saved do |file|
-#  css = File.read(file)
-#  map = file + '.map'
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  map = file + '.map'
 
- # if File.exists? map
-  #  result = AutoprefixerRails.process(css,
-   #  to:   file,
-    #  map:  { prev: File.read(map), inline: false })
-    #File.open(file, 'w') { |io| io << result.css }
-    #File.open(map,  'w') { |io| io << result.map }
-  #else
-   # File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
-  #end
-#end
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+  end
+end
